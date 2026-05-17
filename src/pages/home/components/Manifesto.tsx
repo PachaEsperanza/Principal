064f77d7ace782a127741378fc6bce3df2dba72f
LegadoInca <kjz4kz2db5@privaterelay.appleid.com>
2026-05-17 11:01:32 -0500

    asdad

--- src/pages/home/components/Manifesto.tsx
+++ src/pages/home/components/Manifesto.tsx
@@ -186,8 +186,8 @@ const Manifesto = () => {
         <div className="lg:sticky lg:top-20">
           <div className="rounded-2xl overflow-hidden" style={{ border: '1px solid rgba(201,168,76,0.2)' }}>
             <img
-              src="/SemillasDeEsperanza/images/imagen1.png"
-              alt="Semillas de Esperanza"
+              src="/Home/images/imagen1.png"
+              alt="CompartoTuEsperanza"
               className="w-full h-full object-cover"
               style={{ minHeight: '360px', maxHeight: '480px' }}
             />
